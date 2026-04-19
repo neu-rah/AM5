6f7fbcf936e40833ffaeeb137fef48cb9555d975
neu-rah <ruihfazevedo@gmail.com>
2026-04-19 15:06:41 +0000

    HappyAPI hurray installed high order type transformation
    this is VERY useful for type chaining transformation and API automation

--- RnD/data/src/main.cpp
+++ RnD/data/src/main.cpp
@@ -1,37 +1,108 @@
 #include <iostream>
 using namespace std;
 
+#include <hapi.h>
+
 struct Nil {};
 
-template<typename Cfg=Nil>
-struct API:Cfg {
-  static constexpr void print() {}
+template<typename O=Nil> 
+struct DataAPI:O {
+  using Base=O;
+  using Base::Base;
+  static constexpr bool changed() {return false;}
+  static constexpr void sync() {}
+  // template<typename Out> static void printTo(Out& out,Ctx& ctx) {}
 };
 
-template<typename O,typename T>
-struct App:O::Part<T> {};
-
-template<typename R>
-struct Link {
+template<typename N>
+struct Link:N {
   template<typename O>
-  struct Part:R {
-    static void print() {
-      R::print();
-      O::print();
-    }
+  struct Part:N::Part<O> {
+    using Base=typename N::template Part<O>;
+    using Base::Base;
+    constexpr bool changed() {return Base::changed()||O::changed();}
   };
 };
 
-struct A:Link<A> {
-  static void print() {cout<<"A";}
+template<typename... OO>
+struct DataDef:APIOf<DataAPI<>,OO...>::template Map<Link> {
+  using Base=typename APIOf<DataAPI<>,OO...>::template Map<Link>;
 };
 
-struct B:Link<B> {
-  static void print() {cout<<"B";}
+//compile-time data --
+template<typename T,T data>
+struct StaticData {
+  template<typename O>
+  struct Part:O {
+    using Type=T;
+    using Base=O;
+    using Base::Base;
+    static constexpr T get() {return data;} 
+  };
 };
+
+//runtime data --
+template<typename T,T& data>
+struct StaticRef {
+  template<typename O>
+  struct Part:O {
+    using Type=T;
+    using Base=O;
+    using Base::Base;
+    static constexpr T& get() {return data;} 
+  };
+};
+
+template<typename T>
+struct Data {
+  template<typename O>
+  struct Part:O {
+    using Type=T;
+    using Base=O;
+    using Base::Base;
+    Type data;
+    template<typename... OO> Part(const Type& o,OO&... oo):data{o},Base{oo...}{}
+    constexpr Type& get() {return data;} 
+    constexpr Type& get() const {return data;} 
+  };
+};
+
+struct Watch {
+  template<typename O>
+  struct Part:O {
+    using Base=O;
+    using Type=typename Base::Type;
+    using Base::get;
+    using Base::Base;
+    std::remove_reference_t<Type> watched;
+    constexpr bool changed() {return get()!=watched;}
+    constexpr void sync() {watched=get();}
+  };
+};
+
+//==================================================================--
+const char* nome{"Rui"};
+int ano{1967};
+char notas[]{"Notes..."};
+
+using A=DataDef<StaticData<int,11>>;
+using B=DataDef<StaticRef<const char*,nome>>;
+using C=DataDef<Watch,StaticRef<int,ano>>;
+using D=DataDef<StaticRef<decltype(notas),notas>>;
+
+DataDef<Watch,Data<int>> month{11};
 
 int main() {
-  A::Part<B::Part<API<Nil>>>::print();
-  cout<<endl;
+  month.sync();
+  cout<<"month changed:"<<month.changed()<<endl;
+  cout<<A::get()<<endl;
+  cout<<B::get()<<endl;
+  C::get()=12;
+  cout<<C::get()<<endl;
+  D::get()[0]='V';
+  cout<<D::get()<<endl;
+  month.get()=21;
+  cout<<month.get()<<endl;
+  cout<<"month changed:"<<month.changed()<<endl;
   return 0;
 }
--- include/hapi.h
+++ include/hapi.h
@@ -18,6 +18,7 @@ struct Chain:O {
   using Term=typename O::template Part<typename Chain<OO...>::template Term<T>>;
   template<typename... XX> using Ins=typename ::Chain<XX...,O,OO...>;
   template<typename... XX> using App=typename ::Chain<O,OO...,XX...>;
+  template<template<typename> class M> using Map=Chain<M<O>,M<OO>...>;
 };
 
 template<typename O>
@@ -27,6 +28,7 @@ struct Chain<O>:O {
   using Term=typename O::template Part<T>;
   template<typename... XX> using Ins=typename ::Chain<XX...,O>;
   template<typename... XX> using App=typename ::Chain<O,XX...>;
+  template<template<typename> class M> using Map=M<O>;
 };
 
 template<typename O>
@@ -42,12 +44,16 @@ template<typename API,typename O,typename... OO>
 struct APIOf<API,O,OO... >:Chain<O,OO...>::template Term<API> {
   using Base=typename Chain<O,OO...>::template Term<API>;
   using Base::Base;
+  template<template<typename> class M>
+  using Map=::APIOf<API,M<O>,M<OO>...>;
 };
 
 template<typename API>
 struct APIOf<API>:API {
   using Base=API;
   using Base::Base;
+  template<template<typename> class M>
+  using Map=M<API>;
 };
 
 
