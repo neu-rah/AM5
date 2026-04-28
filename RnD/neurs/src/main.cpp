192847f76516345ca0dfb55cfbd99ee1632e2739
neu-rah <ruihfazevedo@gmail.com>
2026-04-28 18:51:56 +0000

    empty neurs code

---
+++ RnD/neurs/src/main.cpp
@@ -0,0 +1,16 @@
+#include <menu.h>
+
+#ifdef __AVR__
+  #include <streamFlow.h>
+  using namespace StreamFlow;
+  #define cout Serial
+  #include <avr_std.h>
+#else
+  #include <iostream>
+  #include <type_traits>
+#endif
+using namespace std;
+
+int main(){
+  return 0;
+}
