837253763f2cbc166ab97f26fb3cb90dcc12b40c
neu-rah <ruihfazevedo@gmail.com>
2026-05-01 14:06:45 +0000

    remove old code

--- 2026.04.28/src/menu/sys/formats.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "menu/sys/base.h"
-
-template<typename O>
-struct Formats {
-  using IsFormat=std::true_type;
-  static_assert(O::template Excludes<IsPrinter>::value,"Printers must preseed Formats");
-};
