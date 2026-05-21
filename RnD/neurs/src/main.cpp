420cabd1a4eb4f5f170579f6396afffb3f397924
neu-rah <ruihfazevedo@gmail.com>
2026-05-21 13:13:57 +0000

    remove case

--- RnD/neurs/src/main.cpp
+++ RnD/neurs/src/main.cpp
@@ -478,9 +478,6 @@ void setup() {
 int main(){
   // cout<<mainMenu.template has<Id<ids::op3>><<endl;
   setup();
-  nav.go(6);
-  nav.enter();
-  nav.up();
   while(run());
   dout<<xy<0,50><<"end."<<endl;
   return 0;
